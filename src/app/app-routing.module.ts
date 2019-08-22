c2dd38641b3625891c65036025103e1f344e57e8
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-21 21:55:27 -0400

    🚧 work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,30 +7,21 @@ import { QuicklinkStrategy } from 'ngx-quicklink';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => {
-      return import('./dashboard/dashboard.module').then(m => m.DashboardModule);
-    },
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
     pathMatch: 'full',
   },
   {
     path: 'form',
-    loadChildren: () => {
-      return import('./form/form.module').then(m => m.FormModule);
-    },
+    loadChildren: () =>  import('./form/form.module').then(m => m.FormModule),
   },
   {
     path: 'table',
-    loadChildren: () => {
-      return import('./table/table.module').then(m => m.TableModule);
-    },
+    loadChildren: () => import('./table/table.module').then(m => m.TableModule),
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    enableTracing: false,
-    preloadingStrategy: QuicklinkStrategy
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
